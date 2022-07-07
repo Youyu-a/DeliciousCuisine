a46bf80eba3467471cf65b9f2b00eea7002de8e5
mituz <mituzhizhong@outlook.com>
2022-07-07 11:04:30 +0800

    开始

---
+++ src/main/java/com/bdt/dc/DeliciousCuisine.java
@@ -0,0 +1,7 @@
+package com.bdt.dc;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Utils.MOD_ID)
+public class DeliciousCuisine {
+}
---
+++ src/main/java/com/bdt/dc/Utils.java
@@ -0,0 +1,6 @@
+package com.bdt.dc;
+
+public class Utils
+{
+    public static final String MOD_ID = "delicious_cuisine";
+}
