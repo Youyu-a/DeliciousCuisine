72237e0009539a5b2bc8c632a8ac82d82715b226
H <73459149+Youyu-a@users.noreply.github.com>
2022-07-22 18:41:23 +0800

    Delete src/main directory

--- src/main/java/com/bdt/dc/Utils.java
+++
@@ -1,6 +0,0 @@
-package com.bdt.dc;
-
-public class Utils
-{
-    public static final String MOD_ID = "delicious_cuisine";
-}
--- src/main/java/com/bdt/dc/group/ModGroup.java
+++
@@ -1,8 +0,0 @@
-package com.bdt.dc.group;
-
-import net.minecraft.item.ItemGroup;
-
-public class ModGroup
-{
-    public static final ItemGroup foodsGroup = new FoodsGroup();
-}
