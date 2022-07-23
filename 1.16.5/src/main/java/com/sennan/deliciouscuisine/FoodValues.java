3618ed99b0a1d2370e09e850c04d4f4e67f7dfdd
mituz <mituzhizhong@outlook.com>
2022-07-23 14:15:19 +0800

    语言文件

--- 1.16.5/src/main/java/com/sennan/deliciouscuisine/FoodValues.java
+++ 1.16.5/src/main/java/com/sennan/deliciouscuisine/FoodValues.java
@@ -5,4 +5,5 @@ import net.minecraft.item.Food;
 public class FoodValues {
 
     public static final Food EdibleCrops = (new Food.Builder().nutrition(2).saturationMod(0.4f).build());
+    public static final Food NonEdibleCrops = (new Food.Builder().nutrition(0).saturationMod(0).build());
 }
--- 1.16.5/src/main/java/com/sennan/deliciouscuisine/registry/ModFood.java
+++ 1.16.5/src/main/java/com/sennan/deliciouscuisine/registry/ModFood.java
@@ -10,9 +10,12 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ModFood {
     public static final DeferredRegister<Item> FOODS = DeferredRegister.create(ForgeRegistries.ITEMS, Utils.MOD_ID);
 
-    //作物
+    //crop
     public static final RegistryObject<Item> Purple_Sweet_Potato = FOODS.register("purple_sweet_potato",
             () -> new Item(new Item.Properties().food(FoodValues.EdibleCrops).tab(ModGroup.foodGroup)));
     public static final RegistryObject<Item> Sweet_Potato = FOODS.register("sweet_potato",
             () -> new Item(new Item.Properties().food(FoodValues.EdibleCrops).tab(ModGroup.foodGroup)));
+    public static final RegistryObject<Item> Cotton = FOODS.register("cotton",
+            () -> new Item(new Item.Properties().food(FoodValues.NonEdibleCrops).tab(ModGroup.foodGroup)));
+
 }
